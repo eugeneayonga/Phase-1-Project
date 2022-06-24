6b19c063ab8961c6213131f96b11e17285637c3d
Eugene <eugeneayonga@gmail.com>
2022-06-24 12:00:16 +0300

    Initialized variables, created new file

---
+++ script.js
@@ -0,0 +1,5 @@
+// variable initialization
+const cowsPrice = document.querySelector("#bid-count");
+const cowsBar = document.querySelector("#cows-bar");
+const cowsInformation = document.querySelector("#extensive-info");
+const cowsUrl = "http://localhost:3000/cows";
