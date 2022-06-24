c5b3517b33424cf73ebe5f8d812d82954a65eea0
Eugene <eugeneayonga@gmail.com>
2022-06-24 12:11:15 +0300

    fetch request with json to GET cow image, iterate forEach, error catching

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const cowsPrice = document.querySelector("#bid-count");
 const cowsBar = document.querySelector("#cows-bar");
 const cowsInformation = document.querySelector("#extensive-info");
 const cowsUrl = "http://localhost:3000/cows";
+
+
+// making fetch request to add cows to cows bar
+fetch(cowsUrl)
+    .then(response => response.json())
+    .then(data => {
+        let breed = cowsInformation.querySelector("#breed");
+        let image = cowsInformation.querySelector("#image");
+        breed.textContent = cows[0].breed;
+        image.src = cows[0].image;
+        cowsPrice.textContent = cows[0].price;
+
+        cows.forEach(cows => addToCowsBar(cows));
+    })
+    .catch(error => console.log(error));
+    
